092c649432b915ae3254f1d34a5bf21f8f61d09e
Aleksandr Chuklin <a.chuklin@gmail.com>
2018-08-02 09:21:21 +0200

    Workrave integration

---
+++ xmobar-workrave-monitor.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -ex
+
+timer=$(dbus-send --print-reply=literal --type=method_call --dest=org.workrave.Workrave /org/workrave/Workrave/Core org.workrave.CoreInterface.GetTimerElapsed string:'restbreak')
+regex='int32 ([0-9]+)'
+
+if [[ $timer =~ $regex ]] ; then
+    minutes=$((${BASH_REMATCH[1]} / 60))
+    color=grey
+    minutes_left=$((29-minutes))
+    if [[ $minutes_left -lt 0 ]] ; then
+        color='grey,red'
+    fi
+    echo "<fc=$color>break in $minutes_left min</fc>"
+fi
