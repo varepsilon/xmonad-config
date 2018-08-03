381b3ffcb2a665a313ce9e3b23ec7c0948844f39
Aleksandr Chuklin <a.chuklin@gmail.com>
2018-08-03 09:43:38 +0200

    fix

--- xmobar-workrave-monitor.sh
+++ xmobar-workrave-monitor.sh
@@ -7,7 +7,7 @@ regex='int32 ([0-9]+)'
 if [[ $timer =~ $regex ]] ; then
     minutes=$((${BASH_REMATCH[1]} / 60))
     color=grey
-    minutes_left=$((29-minutes))
+    minutes_left=$((60-minutes))
     if [[ $minutes_left -lt 0 ]] ; then
         color='grey,red'
     fi
